15e806f5b8bcddc812bdc689b05ce29bffb09aa6
Matyanson <matyas.perem@seznam.cz>
2022-01-27 15:54:26 +0100

    feat: can switch between multiple translations and the original

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-mapText(document, (text) => uwufy(text));
+translateFunctions = [
+    (text) => text
+    .replace(/[rl]/gi, 'w')
+    .replace(/ove([\s.,])/gi, 'ov$1')
+    .replace(/th([\s.,])/gi, 'ff$1')
+    .replace(/(\w)th(\w)/gi, '$1dd$2')
+    .replace(/!/g, ' òwó.'),
+
+    (text) => text
+    .replace(/(\b|\w{2,})[ny]([aeiou])/gi, '$1ny$2')
+    .replace(/\b([aeu]|[io]\w{3,})/gi, 'ny$1')
+    .replace(/([mn])([aeiou])([mn])/gi, '$1y$2$3')
+    .replace(/([mn])([aiou])/gi, '$1y$2')
+    .replace(/y'/gi, 'ny\'')
+    .replace(/the/gi, 'Nye')
+    .replace(/([.!?])/gi, ' nya~$1')
+]
+
+
+pageHTML = sessionStorage.getItem('page_html');
+i = Number(sessionStorage.getItem('translation_index'));
+
+if(pageHTML == null) {
+    i = 0;
+    pageHTML = document.documentElement.innerHTML;
+    sessionStorage.setItem('translation_index', 0);
+    sessionStorage.setItem('page_html', pageHTML);
+}
+
+document.documentElement.innerHTML = pageHTML;
+
+if(i >= 0)
+    mapText(document, (text) => translateFunctions[i](text));
+
+nextI = i == translateFunctions.length - 1 ? -1 : i + 1;
+sessionStorage.setItem('translation_index', nextI);
 
 function mapText(el, cb) {
     if(el.tagName == 'SCRIPT' || el.tagName == 'STYLE') return;
@@ -8,12 +43,4 @@ function mapText(el, cb) {
         else
             mapText(child, cb);
     }
-}
-function uwufy (text) {
-    return text
-    .replace(/[rl]/gi, 'w')
-    .replace(/ove([\s.,])/gi, 'ov$1')
-    .replace(/th([\s.,])/gi, 'ff$1')
-    .replace(/(\w)th(\w)/gi, '$1dd$2')
-    .replace(/!/g, ' òwó.');
 }
